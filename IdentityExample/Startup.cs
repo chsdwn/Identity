642c0c4bf2f340a12191f5be9eb326431e72649a
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-11-23 14:23:27 +0300

    02. Identity Authentication

---
+++ IdentityExample/Startup.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityExample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(dbContextOptionsBuilder =>
+            {
+                dbContextOptionsBuilder.UseInMemoryDatabase("MemoryDB");
+            });
+
+            // AddIdentity registers the services
+            services.AddIdentity<IdentityUser, IdentityRole>(identityOptions =>
+            {
+                identityOptions.Password.RequiredLength = 4;
+                identityOptions.Password.RequireDigit = false;
+                identityOptions.Password.RequireNonAlphanumeric = false;
+                identityOptions.Password.RequireUppercase = false;
+                identityOptions.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(cookieAuthenticationOptions =>
+            {
+                cookieAuthenticationOptions.Cookie.Name = "Identity.Cookie";
+                cookieAuthenticationOptions.LoginPath = "/Home/Login";
+            });
+
+            // services.AddAuthentication("CookieAuth")
+            //     .AddCookie("CookieAuth", config =>
+            //     {
+            //         config.Cookie.Name = "Grandmas.Cookie";
+            //         // Default "/Account/Login"
+            //         config.LoginPath = "/Home/Authenticate";
+            //     });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
